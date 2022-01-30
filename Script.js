42523f136d5666a28cff36706a96072498d10ff8
Florexianer <8111@htl.rennweg.at>
2022-01-30 18:59:56 +0100

    Core User Story 5 -> !! no responsive Framework !!

--- Script.js
+++ Script.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const displayObj = document.getElementById("fetched-data")
+    const randomButton = document.getElementById("random")
 
-
-    fetch('data.json')
-        .then(response => response.json())
-        .then(data => {
-            displayObj.innerHTML = JSON.stringify(data);
-        });
+    randomButton.onclick = function () {
+        fetch('data.json')
+            .then(response => response.json())
+            .then(data => {
+                displayObj.innerHTML = JSON.stringify(data[Math.floor(Math.random() * 13946)]);
+            });
+    }
 });
-
-
-
