8b660bef3dc9f35c6d72cefc3e830dcd95b35b18
Florexianer <8111@htl.rennweg.at>
2022-01-30 20:51:08 +0100

    Core finished

--- Script.js
+++ Script.js
@@ -1,13 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const displayObj = document.getElementById("fetched-data")
+    const Title = document.getElementById("Title")
+    const Province = document.getElementById("Province")
+    const Description = document.getElementById("Description")
+
     const randomButton = document.getElementById("random")
 
     randomButton.onclick = function () {
         fetch('data.json')
             .then(response => response.json())
             .then(data => {
-                displayObj.innerHTML = JSON.stringify(data[Math.floor(Math.random() * 13946)]);
+                let currentRandom = data[Math.floor(Math.random() * 13946)]
+                console.log(currentRandom)
+                for (key in currentRandom) {
+                    if (currentRandom.hasOwnProperty(key)) {
+                        if(key == "Titel") {
+                            Title.innerHTML =currentRandom[key];
+                        } else if (key == "Province") {
+                            Province.innerHTML = "Bundesland : " +currentRandom[key];
+                        } else if (key == "Description"){
+                            Description.innerHTML = currentRandom[key];
+                        }
+                    }
+                }
             });
     }
 });
