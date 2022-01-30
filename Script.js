32ef0f0f952681af00faf0d479a569aeedb277f3
Florexianer <8111@htl.rennweg.at>
2022-01-30 17:22:29 +0100

    Story 3, laggy watch out

--- Script.js
+++ Script.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const displayDiv = document.getElementById("fetched-data")
+    const displayObj = document.getElementById("fetched-data")
 
 
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    fetch('data.json')
         .then(response => response.json())
         .then(data => {
-            for (const dataKey in data) {
-                displayDiv.innerHTML +=dataKey + " : " + data[dataKey] + "<br>"
-            }
+            displayObj.innerHTML = JSON.stringify(data);
         });
 });
 
