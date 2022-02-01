f753376e4ddae52a2a2b4d68ef33d0cbd1e8a3f2
Florexianer <8111@htl.rennweg.at>
2022-02-01 07:55:03 +0100

    minor changes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# career_roller
+# Career Roller
+### A tool that helps people who are unsatisfied with their job to choose a random new career path.
+
+<br> 
+
+##### Made by Harman Dhanoya and Florian Kozel
+##### Data provided by WIFI Vienna
--- Script.js
+++ Script.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const Title = document.getElementById("Title")
     const Province = document.getElementById("Province")
     const Description = document.getElementById("Description")
-
     const randomButton = document.getElementById("random")
 
     randomButton.onclick = function () {
@@ -11,18 +10,20 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(response => response.json())
             .then(data => {
                 let currentRandom = data[Math.floor(Math.random() * 13946)]
-                console.log(currentRandom)
                 for (key in currentRandom) {
                     if (currentRandom.hasOwnProperty(key)) {
-                        if(key == "Titel") {
-                            Title.innerHTML =currentRandom[key];
-                        } else if (key == "Province") {
-                            Province.innerHTML = "Bundesland : " +currentRandom[key];
-                        } else if (key == "Description"){
+                        if (key === "Titel") {
+                            Title.innerHTML = currentRandom[key];
+                        } else if (key === "Province") {
+                            Province.innerHTML = "Bundesland : " + currentRandom[key];
+                        } else if (key === "Description") {
                             Description.innerHTML = currentRandom[key];
                         }
                     }
                 }
+            })
+            .catch(error => {
+                console.error('Error:', error);
             });
     }
 });
